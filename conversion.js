952569bb2773eb7460e832c137365c6472b37f08
Go Hit <godymg71@gmail.com>
2025-11-13 16:36:04 +0300

    javascript

---
+++ conversion.js
@@ -0,0 +1,25 @@
+let num="23.5"
+
+let x=Number(num)
+let y=parseFloat(num)
+let z=parseInt(num)
+console.log(num,typeof(num))
+console.log(x,typeof(x))
+console.log(y,typeof(y))
+console.log(z,typeof(z))
+
+let a=598
+let b=String(a)
+let c=a.toString()
+console.log(a,typeof(a))
+console.log(b,typeof(b))
+console.log(c,typeof(c))
+
+//type coercion
+let value1="5"
+let value2=12
+console.log(value1+value2)
+
+let valu1="5"
+let valu2="12"
+console.log(valu1/valu2)
